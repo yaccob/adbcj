cedb3266c1276a172f64b4808079f5c1b7d9607f
Roman Stoffel <roman.stoffel@gamlor.info>
2012-11-22 13:16:34 +0100

    Bring back Mysql initialiseer

---
+++ tck/src/test/java/org/adbcj/tck/InitMysql.java
@@ -0,0 +1,21 @@
+package org.adbcj.tck;
+
+/**
+ * @author roman.stoffel@gamlor.info
+ */
+public class InitMysql extends InitDatabase {
+    @Override
+    protected void loadDriver() throws ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+    }
+
+    @Override
+    protected String setupScript() {
+        return "sql/setupMySQL.sql";
+    }
+
+    @Override
+    protected String tearDownScript() {
+        return "sql/cleanUpMySQL.sql";
+    }
+}
