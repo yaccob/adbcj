ec12ebab1160a4d8602f184301da3f34d1b5b32b
Roman Stoffel <roman.stoffel@gamlor.info>
2013-02-27 14:24:26 +0100

    - Changed stack trace to work with Java 6

--- tck/src/test/java/org/adbcj/tck/test/AbstractWithConnectionManagerTest.java
+++ tck/src/test/java/org/adbcj/tck/test/AbstractWithConnectionManagerTest.java
@@ -26,7 +26,7 @@ public abstract class AbstractWithConnectionManagerTest {
     }
 
     protected Map<String,String> properties(){
-        return new HashMap<>();
+        return new HashMap<String,String>();
     }
 
     @AfterClass
