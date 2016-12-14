21d5b2b6237cca79b0d71837d6461c4a7f007a12
Roman Stoffel <roman.stoffel@gamlor.info>
2016-12-14 20:56:03 +0900

    Remove connection pool. Will be implemented in complete different fashion.

--- connection-pool/src/main/java/org/adbcj/connectionpool/PooledResource.java
+++
@@ -1,8 +0,0 @@
-package org.adbcj.connectionpool;
-
-/**
- * Marks instances as pooled resources. Makes it easier to check if we wrapped all methods.
- * @author roman.stoffel@gamlor.info
- */
-interface PooledResource {
-}
