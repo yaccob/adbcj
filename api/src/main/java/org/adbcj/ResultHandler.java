8963646daeca0ef4b12f12a2852bc1c5b24abb89
Roman Stoffel <roman.stoffel@gamlor.info>
2017-09-22 23:35:08 +0900

    Remove useless Javadoc tags

--- api/src/main/java/org/adbcj/ResultHandler.java
+++ api/src/main/java/org/adbcj/ResultHandler.java
@@ -30,59 +30,41 @@ public interface ResultHandler<T> {
 
 	/**
 	 * Invoked when field definitions are about to be received.
-	 * 
-	 * @param accumulator
 	 */
 	void startFields(T accumulator);
 	
 	/**
 	 * Invoked for each field definition.
-	 * 
-	 * @param field the field definition
-	 * @param accumulator
 	 */
 	void field(Field field, T accumulator);
 	
 	/**
 	 * Invoked when all field definitions have been received.
-	 * 
-	 * @param accumulator
 	 */
 	void endFields(T accumulator);
 
 	/**
 	 * Invoked when rest rows are about to be received.
-	 *
-	 * @param accumulator
 	 */
 	void startResults(T accumulator);
 	
 	/**
 	 * Invoked at the beginning of a data row.
-	 * 
-	 * @param accumulator
 	 */
 	void startRow(T accumulator);
 	
 	/**
 	 * Invoked for each column in a data row.
-	 * 
-	 * @param value the value of the column 
-	 * @param accumulator
 	 */
 	void value(Value value, T accumulator);
 	
 	/**
 	 * Invoked at the end of a data row.
-	 * 
-	 * @param accumulator
 	 */
 	void endRow(T accumulator);
 	
 	/**
 	 * Invoked after all the data rows have been processed.
-	 * 
-	 * @param accumulator
 	 */
 	void endResults(T accumulator);
 
