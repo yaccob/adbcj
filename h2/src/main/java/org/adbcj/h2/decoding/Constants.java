cf0c3aa7cf674bd9883064393aa26cad129d4231
Roman Stoffel <roman.stoffel@gamlor.info>
2017-10-19 22:15:48 +0900

    Remove commenting the obvious.

--- h2/src/main/java/org/adbcj/h2/decoding/Constants.java
+++ h2/src/main/java/org/adbcj/h2/decoding/Constants.java
@@ -1,20 +1,12 @@
 package org.adbcj.h2.decoding;
 
 public final class Constants {
-    /**
-     * The TCP protocol version number 11.
-     */
     public static final int TCP_PROTOCOL_VERSION_12 = 12;
-    
-    /**
-     * The TCP protocol version number 14.
-     */
+
+
     public static final int TCP_PROTOCOL_VERSION_14 = 14;
-    
-    /**
-     * The TCP protocl version number 15.
-     * @since 2017-09-24 little-pan
-     */
+
+
     public static final int TCP_PROTOCOL_VERSION_15 = 15;
     
     
